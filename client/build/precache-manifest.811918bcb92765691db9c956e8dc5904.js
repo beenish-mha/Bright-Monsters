897081a541739c3fa641576ad1fdc7dd39a26277
beenish <beeni.khurram@gmail.com>
2020-04-07 20:52:38 +0100

    connecting with react, mongoDB and node

---
+++ client/build/precache-manifest.811918bcb92765691db9c956e8dc5904.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "532530096da077b29780d711acdd02b9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "372f2fd942bbcd82a784",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bbde3c166b8249b022d3",
+    "url": "/static/js/2.c149ac25.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c149ac25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "372f2fd942bbcd82a784",
+    "url": "/static/js/main.acb77a24.chunk.js"
+  },
+  {
+    "revision": "549e3a6808490149cae6",
+    "url": "/static/js/runtime-main.ad89515a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
