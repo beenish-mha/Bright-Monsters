ebce7ebed7674a15b7f316000c884fe8be4c490a
beenish <beeni.khurram@gmail.com>
2020-04-09 21:19:45 +0100

    Port defined by environmental variable

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const routes = require("./routes/index");
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
