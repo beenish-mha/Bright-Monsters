b1e106fa4fb1724903a2a38f8020df323236008a
beenish <beeni.khurram@gmail.com>
2020-04-10 18:11:19 +0100

    creating Main page and applying material-ui framework

--- client/src/components/MainPage.js
+++ client/src/components/MainPage.js
@@ -1,14 +1,53 @@
 import React from "react";
 import AboutUs from "./AboutUs";
+import NavBar from "./NavBar";
+import { Container, makeStyles, Paper, Grid } from "@material-ui/core";
+
+const useStyle = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+  },
+  containerStyle: {
+    backgroundColor: "lightblue",
+  },
+}));
 
 function Mainpage() {
+  const classes = useStyle();
   return (
-    <div>
-      {" "}
-      <h2>Bright Monsters!</h2>
-      <AboutUs />
+    <div className={classes.root}>
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <Paper className={classes.paper}>
+            <h2>Bright Monsters!</h2>
+          </Paper>
+        </Grid>
+        <Grid item xs={6} sm={3}>
+          <Paper className={classes.paper}>
+            <NavBar />
+          </Paper>
+        </Grid>
+        <Grid item xs={6} sm={9}>
+          <Paper className={classes.paper}>
+            <AboutUs />
+          </Paper>
+        </Grid>
+      </Grid>
     </div>
   );
+
+  // return (
+  //   <Container className={classes.containerStyle} maxWidth="lg">
+  //     {" "}
+  //     <NavBar />
+  //     <AboutUs />
+  //   </Container>
+  // );
 }
 
 export default Mainpage;
