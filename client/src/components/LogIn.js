7823119c463cdf2b9c5038c409f3eaea189de6a1
beenish <beeni.khurram@gmail.com>
2020-04-11 11:17:39 +0100

    created basic form for login

--- client/src/components/LogIn.js
+++ client/src/components/LogIn.js
@@ -1,7 +1,71 @@
 import React from "react";
+import { TextField, Button } from "@material-ui/core";
+import API from "../utils/Api";
 
-function LogIn() {
-  return <h2>this is login page</h2>;
+class LogIn extends React.Component {
+  state = {
+    email: "",
+    password: "",
+  };
+
+  //   submit = (event) => {
+  //     event.preventDefault();
+
+  //     API.saveUser({
+  //       name: this.state.name,
+  //       email: this.state.email,
+  //       password: this.state.password,
+  //     })
+  //       .then((response) => {
+  //         alert("Add kids");
+  //       })
+  //       .catch((err) => console.log(err));
+  //   };
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  render() {
+    console.log(this.state.password);
+    return (
+      <div>
+        <h6>Log In</h6>
+        <form
+          onSubmit={this.submit}
+          className="form"
+          noValidate
+          autoComplete="off"
+        >
+          <div>
+            <TextField
+              required
+              id="standard-required"
+              label="E-Mail"
+              value={this.state.email}
+              name="email"
+              onChange={this.handleChange}
+            />
+            <br />
+            <TextField
+              required
+              id="standard-password-input"
+              label="Password"
+              type="password"
+              value={this.state.password}
+              name="password"
+              onChange={this.handleChange}
+            />
+            <br />
+            <button className="btn">Enter</button>
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default LogIn;
