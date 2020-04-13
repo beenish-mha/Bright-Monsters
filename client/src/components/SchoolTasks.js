16ea45441bcf9675c382f64e2e7c577e034cf9a6
beenish <beeni.khurram@gmail.com>
2020-04-13 16:50:22 +0100

    added buttons after text field

--- client/src/components/SchoolTasks.js
+++ client/src/components/SchoolTasks.js
@@ -1,12 +1,17 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import TextField from "@material-ui/core/TextField";
-
+import Button from "@material-ui/core/Button";
 const useStyles = makeStyles((theme) => ({
   root: {
     "& > *": {
       margin: theme.spacing(1),
       width: "80ch",
+    },
+  },
+  btnRoot: {
+    "& > *": {
+      margin: theme.spacing(1),
     },
   },
 }));
@@ -18,10 +23,36 @@ function SchoolTasks() {
       <p>school tasks will go here</p>
       <form className={classes.root} noValidate autoComplete="off">
         <TextField id="outlined-basic" variant="outlined" />
+        <div className={classes.btnRoot}>
+          <Button variant="contained">Add</Button>
+          <Button variant="contained">Delete</Button>
+          <Button variant="contained">Get Reward</Button>
+        </div>
+
         <TextField id="outlined-basic" variant="outlined" />
+        <div className={classes.btnRoot}>
+          <Button variant="contained">Add</Button>
+          <Button variant="contained">Delete</Button>
+          <Button variant="contained">Get Reward</Button>
+        </div>
         <TextField id="outlined-basic" variant="outlined" />
+        <div className={classes.btnRoot}>
+          <Button variant="contained">Add</Button>
+          <Button variant="contained">Delete</Button>
+          <Button variant="contained">Get Reward</Button>
+        </div>
         <TextField id="outlined-basic" variant="outlined" />
+        <div className={classes.btnRoot}>
+          <Button variant="contained">Add</Button>
+          <Button variant="contained">Delete</Button>
+          <Button variant="contained">Get Reward</Button>
+        </div>
         <TextField id="outlined-basic" variant="outlined" />
+        <div className={classes.btnRoot}>
+          <Button variant="contained">Add</Button>
+          <Button variant="contained">Delete</Button>
+          <Button variant="contained">Get Reward</Button>
+        </div>
       </form>
     </div>
   );
